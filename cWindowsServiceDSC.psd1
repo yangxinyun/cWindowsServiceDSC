b202b24e5a1b0f299f4c758080076988fb928c43
yangxinyun <yangxinyun204@gmail.com>
2021-07-15 16:39:26 +0200

    first draft of the source code

---
+++ cWindowsServiceDSC.psd1
@@ -0,0 +1,58 @@
+@{
+    RootModule           = 'cWindowsServiceDSC.psm1'
+    DscResourcesToExport = @('WindowsServiceDSC')
+
+    # Version number of this module.
+    ModuleVersion        = '0.0.1'
+
+    # ID used to uniquely identify this module
+    GUID                 = 'e74c78aa-62ec-41ab-992b-9928d97dd530'
+
+    # Author of this module
+    Author               = 'Yang Xinyun'
+
+    # Company or vendor of this module
+    CompanyName          = 'Yang Xinyun'
+
+    # Copyright statement for this module
+    Copyright            = '(c) 2021 Yang Xinyun. All rights reserved.'
+
+    # Description of the functionality provided by this module
+    Description          = 'Powershell DSC Resource to configure Windows service startup type and Recovery.'
+
+    # Minimum version of the Windows PowerShell engine required by this module
+    PowerShellVersion    = '5.1'
+
+    PrivateData          = @{
+
+        PSData = @{
+    
+            # Tags applied to this module. These help with module discovery in online galleries.
+            Tags                     = @('DSC', 'DSCResource', 'DesiredStateConfiguration', 'WindowsService')
+    
+            # A URL to the license for this module.
+            # LicenseUri = ''
+    
+            # A URL to the main website for this project.
+            ProjectUri               = 'https://github.com/yangxinyun/cWindowsServiceDSC'
+    
+            # A URL to an icon representing this module.
+            IconUri                  = 'https://toppng.com/uploads/preview/ear-icon-free-windows-service-ico-11563265466nw6vqap7gv.png'
+    
+            # ReleaseNotes of this module
+            # ReleaseNotes = ''
+    
+            # Prerelease string of this module
+            # Prerelease = ''
+    
+            # Flag to indicate whether the module requires explicit user acceptance for install/update/save
+            RequireLicenseAcceptance = $false
+    
+            # External dependent modules of this module
+            # ExternalModuleDependencies = @()
+    
+        } # End of PSData hashtable
+    
+    }
+
+}
---
+++ cWindowsServiceDSC.psm1
@@ -0,0 +1,194 @@
+﻿enum Ensure
+{ 
+    Absent
+    Present
+}
+
+enum StartupType
+{ 
+    Automatic
+    AutomaticDelayed
+    Disabled
+    Manual
+}
+
+enum RecoveryType
+{
+    Restart
+    Reboot
+}
+
+function Set-CurrentRecoveryType
+{
+    param($original, [WindowsServiceDSC]$strongTypedOutput)
+
+    switch -Wildcard ($original)
+    {
+        "*RESTART*"
+        {
+            $strongTypedOutput.ServiceCurrentRecoveryType += 'Restart'
+            break
+        }
+        "*REBOOT*"
+        {
+            $strongTypedOutput.ServiceCurrentRecoveryType += 'Reboot'
+            break
+        }
+    }
+}
+
+[DscResource()]
+class WindowsServiceDSC
+{
+    [DscProperty(Key)]
+    [String]$ServiceName
+
+    [DscProperty(NotConfigurable)]
+    [Ensure]$ServiceExists
+
+    [DscProperty(NotConfigurable)]
+    [StartupType]$ServiceCurrentStartupType
+
+    [DscProperty(NotConfigurable)]
+    [RecoveryType[]]$ServiceCurrentRecoveryType
+
+    [DscProperty(Mandatory = $true)]
+    [Ensure]$Ensure
+
+    [DscProperty(Mandatory = $false)]
+    [RecoveryType]$RecoveryType = [RecoveryType]::Restart
+
+    [DscProperty(Mandatory = $false)]
+    [StartupType]$StartupType = [StartupType]::Automatic
+
+    # Gets the resource's current state.
+    [WindowsServiceDSC] Get()
+    {
+        try
+        {
+            Get-Service $this.ServiceName -ErrorAction 'stop'
+            $this.ServiceExists = 'Present'
+
+            $servieConfig = (sc.exe qc "$($this.ServiceName)")[4].trim()
+            switch ($servieConfig)
+            {
+                "START_TYPE         : 2   AUTO_START"
+                {
+                    $this.ServiceCurrentStartupType = 'Automatic'
+                }
+                "START_TYPE         : 2   AUTO_START  (DELAYED)"
+                {
+                    $this.ServiceCurrentStartupType = 'AutomaticDelayed'
+                }
+                "START_TYPE         : 3   DEMAND_START"
+                {
+                    $this.ServiceCurrentStartupType = 'Manual'
+                }
+                "START_TYPE         : 4   DISABLED"
+                {
+                    $this.ServiceCurrentStartupType = 'Disabled'
+                }
+            }
+
+            $serviceRecoveryConfig = (sc.exe qfailure "$($this.ServiceName)").trim()
+            #First Failure Recovery
+            Set-CurrentRecoveryType $serviceRecoveryConfig[6] ([ref]$this)
+
+            #Second Failure Recovery
+            Set-CurrentRecoveryType $serviceRecoveryConfig[7] ([ref]$this)
+            
+            #Third Failure Recovery
+            Set-CurrentRecoveryType $serviceRecoveryConfig[8] ([ref]$this)
+        }
+        catch #[Microsoft.PowerShell.Commands.ServiceCommandException] Cannot find this exception type
+        {
+            $this.ServiceExists = 'Absent'
+        }
+        return $this
+    }
+    
+    # Sets the desired state of the resource.
+    [void] Set()
+    {
+        Write-Verbose "Desired State is: $($this.ServiceName) $($this.Ensure)"
+        Write-Verbose "Desired State is: $($this.ServiceName) StartupType $($this.StartupType))"
+        Write-Verbose "Desired State is: $($this.ServiceName) RecoveryType $($this.RecoveryType)"
+
+        $ServiceInfo = $this.Get()
+        if ($ServiceInfo.ServiceCurrentStartupType -ne $this.StartupType)
+        {
+            Write-Verbose "Setting $($this.ServiceName) StartupType to  $($this.StartupType)"
+            
+            switch ($this.StartupType)
+            {
+                'Automatic'
+                {
+                    sc.exe config ($this.ServiceName) start= auto | Write-Verbose
+                    break
+                }
+                'AutomaticDelayed'
+                {
+                    sc.exe config ($this.ServiceName) start= delayed-auto | Write-Verbose
+                    break
+                }
+                'Disabled'
+                {
+                    sc.exe config ($this.ServiceName) start= disabled | Write-Verbose
+                    break
+                }
+                'Manual'
+                {
+                    sc.exe config ($this.ServiceName) start= demand | Write-Verbose
+                    break
+                }
+            }
+        }
+
+        if ($ServiceInfo.ServiceCurrentRecoveryType.Where{ $_ -notin $this.RecoveryType } -or $ServiceInfo.ServiceCurrentRecoveryType.Where{ ![string]::IsNullOrWhiteSpace($_) }.count -ne 3)
+        {
+            Write-Verbose "Setting $($this.ServiceName) RecoveryType to $($this.RecoveryType)"
+            
+            switch ($this.RecoveryType)
+            {   
+                'Restart'
+                {
+                    sc.exe failure ($this.ServiceName) reset= 3600 actions= restart/60000/restart/60000/restart/60000 | Write-Verbose
+                    break
+                }
+                'REBOOT'
+                {
+                    sc.exe failure ($this.ServiceName) reset= 3600 actions= reboot/60000/reboot/60000/reboot/60000 | Write-Verbose
+                    break
+                }
+            }
+        }
+    }
+    
+    # Tests if the resource is in the desired state.
+    [bool] Test()
+    {
+        $ServiceInfo = $this.Get()
+        if ($ServiceInfo.ServiceExists -ne $this.Ensure)
+        {
+            Write-Verbose "$($this.ServiceName) expects to be $($this.Ensure), But $($ServiceInfo.ServiceExists)"
+            return $false
+        }
+
+        if ($ServiceInfo.ServiceCurrentStartupType -ne $this.StartupType)
+        {
+            Write-Verbose "$($this.ServiceName) StartupType expects to be $($this.StartupType), But $($ServiceInfo.ServiceCurrentStartupType)"
+
+            return $false
+        }
+
+        if ($ServiceInfo.ServiceCurrentRecoveryType.Where{ $_ -notin $this.RecoveryType } -or $ServiceInfo.ServiceCurrentRecoveryType.Where{ ![string]::IsNullOrWhiteSpace($_) }.count -ne 3)
+        {
+            Write-Verbose "$($this.ServiceName) RecoveryType expects to be $($this.RecoveryType), But $($ServiceInfo.ServiceCurrentRecoveryType)"
+
+            return $false
+        }
+
+        Write-Verbose "$($this.ServiceName) RecoveryType: $($this.ServiceCurrentRecoveryType):$($this.ServiceCurrentRecoveryType.count), StartupType: $($this.ServiceCurrentStartupType)"
+        return $true
+    }
+}
